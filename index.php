adbc7c26fc154b3eee2a0562bb0e9129d1f819e4
dean5201314 <smile90624@gmail.com>
2023-10-27 09:02:24 +0800

    git init

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>網頁傳值</title>
+</head>
+
+<body>
+    <form action="">
+        <div>
+            <label for="acc"></label>
+            <input type="text" name="acc" id="acc">
+        </div>
+        <div>
+            <label for="pw"></label>
+            <input type="password" name="pw" id="pw">
+        </div>
+        <div>
+            <label for="submit"></label>
+            <input type="submit" name="acc" id="acc">
+        </div>
+
+    </form>
+</body>
+
+</html>
